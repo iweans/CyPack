63d7d45609ea2a8bad7b078751172c43b314e9da
renlintao <renlintao@matrixtime.com>
2022-06-23 19:46:10 +0800

    update

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import os
 import os.path
+from distutils.core import setup
 from pprint import pprint
+import shutil
 # ------------------------------
+import Cython
+import Cython.Build
 # ------------------------------
 from repo import get_git_repo, parse_git_version
-from utils import copy_project_to
+from utils import copy_project_to, search_tocompile_files
 # ----------------------------------------
 
 
@@ -39,3 +43,23 @@ pprint(git_version_info)
 
 copy_project_to(target_project_dir, target_package_dir, flag_using_link)
 
+exclude_file_relpath_list = ["top_vision.py"]
+exclude_dir_relpath_list = ["algorithms"]
+tocompile_file_list = search_tocompile_files(target_package_dir, 
+                                             exclude_files=[os.path.join(target_package_dir, exclude_file_relpath) 
+                                                            for exclude_file_relpath in exclude_file_relpath_list],
+                                             exclude_dirs=[os.path.join(target_package_dir, exclude_dir_relpath)
+                                                           for exclude_dir_relpath in exclude_dir_relpath_list])
+
+pprint(tocompile_file_list)
+
+os.chdir(target_package_dir)
+cythonized_extension = Cython.Build.cythonize(tocompile_file_list, 
+                                              compiler_directives={'language_level': "3"})
+
+for filepath in tocompile_file_list:
+    os.remove(filepath)
+
+setup(ext_modules=cythonized_extension)
+
+shutil.rmtree(os.path.join(target_package_dir, "build"))
--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 import os
 import os.path
+import glob
 import shutil
+
+from isort import file
 
 
 def copy_project_to(src_dir: str, dst_dir: str,
@@ -41,3 +44,39 @@ def copy_project_to(src_dir: str, dst_dir: str,
             else:
                 shutil.copy(src_filepath, dst_filepath)
     return True
+
+
+def search_tocompile_files(search_dir: str, 
+                           exclude_files=None, exclude_dirs=None) -> list:
+    exclude_file_list = exclude_files or []
+    exclude_dir_list = exclude_dirs or []
+
+    search_ext_list = [
+        "py", "pY", "Py", "PY", 
+        "pyx", "pyX", "pYx", "pYX", "Pyx", "PyX", "PYx", "PYX",
+    ]
+
+    tocompile_file_list = []
+    for search_ext in search_ext_list:
+        search_glob_pattern = os.path.join(search_dir, f"**/*.{search_ext}")
+        for filepath in glob.iglob(search_glob_pattern, recursive=True):
+            filename = os.path.basename(filepath)
+            lowcase_filename = filename.lower()
+            if (lowcase_filename == "setup.py") \
+                    or (lowcase_filename == "__init__.py"):
+                continue
+            
+            if filepath in exclude_file_list:
+                continue
+            flag_in_exclude_dir = False
+            for exclude_dirpath in exclude_dir_list:
+                if filepath.startswith(exclude_dirpath):
+                    flag_in_exclude_dir = True
+                    break
+            if flag_in_exclude_dir:
+                continue
+
+            tocompile_file_list.append(filepath)
+
+    return tocompile_file_list
+
