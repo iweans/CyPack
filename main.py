625f5a8a0ee67e60ad8a16cff4c5391acb3911c3
renlintao <renlintao@matrixtime.com>
2022-06-25 17:59:00 +0800

    update

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 import os
+import time
+import datetime
 import os.path
+from distutils.extension import Extension
 from distutils.core import setup
 from pprint import pprint
 import shutil
 # ------------------------------
 import Cython
 import Cython.Build
+from matplotlib.pyplot import flag
 # ------------------------------
 from repo import get_git_repo, parse_git_version
 from utils import copy_project_to, search_tocompile_files
 # ----------------------------------------
 
 
-TARGET_PROJECT_DIR = "/data/projects/wai4/MXT_QC_TopTally"
-TARGET_PACKAGE_DIR = "/home/jskj/Desktop/tmp/MXT_QC_TopTally"
+start_dt = datetime.datetime.now()
+
+
+TARGET_PROJECT_DIR = "/home/jskj/Desktop/tmp/MXT_QC_TopTally"
+TARGET_PACKAGE_DIR = "/home/jskj/Desktop/tmp/package"
 
 
 target_project_dir = os.path.abspath(TARGET_PROJECT_DIR)
@@ -21,6 +28,9 @@ if not os.path.isdir(target_project_dir):
     print("目标项目路径必须是一个目录")
     exit()
 target_package_dir = os.path.abspath(TARGET_PACKAGE_DIR)
+if os.path.exists(target_package_dir):
+    os.rename(target_package_dir, 
+              target_package_dir+f"-{start_dt.strftime('%Y%m%d%H%M%S.%f')}")
 os.makedirs(target_package_dir)
 
 target_project_dir_stat = os.stat(target_project_dir)
@@ -41,25 +51,46 @@ else:
 pprint(git_version_info)
 
 
-copy_project_to(target_project_dir, target_package_dir, flag_using_link)
+flag_root_dir_has_init_pyfile \
+    = copy_project_to(target_project_dir, target_package_dir, flag_using_link)
+
+assert flag_root_dir_has_init_pyfile == True
 
 exclude_file_relpath_list = ["top_vision.py"]
-exclude_dir_relpath_list = ["algorithms"]
+# exclude_dir_relpath_list = ["algorithms"]
+exclude_dir_relpath_list = []
 tocompile_file_list = search_tocompile_files(target_package_dir, 
                                              exclude_files=[os.path.join(target_package_dir, exclude_file_relpath) 
                                                             for exclude_file_relpath in exclude_file_relpath_list],
                                              exclude_dirs=[os.path.join(target_package_dir, exclude_dir_relpath)
                                                            for exclude_dir_relpath in exclude_dir_relpath_list])
 
-pprint(tocompile_file_list)
 
+def cythonize_files(target_package_dir: str, to_compile_file_list: list) -> list:
+    pprint(to_compile_file_list)
+    orig_cwd = os.getcwd()
+    os.chdir(target_package_dir)
+
+    cythonized_extension_list = []
+    for tocompile_file in tocompile_file_list:
+        cythonized_extension = Cython.Build.cythonize(tocompile_file, 
+                                                      compiler_directives={'language_level': "3"})
+        cythonized_extension_list.extend(cythonized_extension)
+    
+    os.chdir(orig_cwd)
+    return cythonized_extension_list
+
+
+cythonized_extension_list = cythonize_files(target_package_dir, tocompile_file_list)
+
+if flag_root_dir_has_init_pyfile:
+    os.rename(os.path.join(target_package_dir, "__init__pyfile"),
+              os.path.join(target_package_dir, "__init__.py"))
 os.chdir(target_package_dir)
-cythonized_extension = Cython.Build.cythonize(tocompile_file_list, 
-                                              compiler_directives={'language_level': "3"})
+setup(ext_modules=cythonized_extension_list)
+
 
 for filepath in tocompile_file_list:
     os.remove(filepath)
-
-setup(ext_modules=cythonized_extension)
 
 shutil.rmtree(os.path.join(target_package_dir, "build"))
--- utils.py
+++ utils.py
@@ -6,16 +6,25 @@ import shutil
 from isort import file
 
 
+_tocompile_ext_list = [
+    "py", "pY", "Py", "PY", 
+    "pyx", "pyX", "pYx", "pYX", "Pyx", "PyX", "PYx", "PYX",
+]
+
+
 def copy_project_to(src_dir: str, dst_dir: str,
                     using_link=False) -> bool:
     """
     """
     # ----------------------------------------
     # ------------------------------
+    flag_root_dir_has_init_pyfile = False
     hidden_reldirpath_stack = []
     for src_dirpath, src_subdirname_list, src_subfilename_list in os.walk(src_dir):
         src_dirname = os.path.basename(src_dirpath)
         src_reldirpath = os.path.relpath(src_dirpath, start=src_dir)
+
+        flag_is_root_dir = (src_reldirpath == ".")
         # --------------------
         if len(hidden_reldirpath_stack):
             if src_reldirpath.startswith(hidden_reldirpath_stack[-1]):
@@ -30,10 +39,21 @@ def copy_project_to(src_dir: str, dst_dir: str,
         # --------------------
         # 创建对应的目录
         dst_dirpath = os.path.join(dst_dir, src_reldirpath)
-        if src_reldirpath != ".":
+        if not flag_is_root_dir:
             os.mkdir(dst_dirpath)
+
         # 复制目录的子文件
+        flag_has_init_pyfile = False
+        flag_has_tocompile_file = False
         for src_filename in src_subfilename_list:
+            pure_name, ext = os.path.splitext(src_filename)
+            if src_filename == "__init__.py":
+                flag_has_init_pyfile = True
+                if flag_is_root_dir:
+                    flag_root_dir_has_init_pyfile = True
+            elif len(ext)>1 \
+                    and ext[1:] in _tocompile_ext_list:
+                flag_has_tocompile_file = True
             src_filepath = os.path.join(src_dirpath, src_filename)
             dst_filepath = os.path.join(dst_dirpath, src_filename)
             if os.path.islink(src_filepath):
@@ -43,23 +63,36 @@ def copy_project_to(src_dir: str, dst_dir: str,
                 os.link(src_filepath, dst_filepath)
             else:
                 shutil.copy(src_filepath, dst_filepath)
-    return True
+        
+        if not flag_is_root_dir \
+                and not flag_has_init_pyfile \
+                and flag_has_tocompile_file:
+            dst_init_py_file_path = os.path.join(dst_dirpath, "__init__.py")
+            with open(dst_init_py_file_path, "w") as f:
+                f.flush()
+        
+        if flag_is_root_dir \
+                and flag_root_dir_has_init_pyfile:
+            os.rename(os.path.join(dst_dirpath, "__init__.py"),
+                      os.path.join(dst_dirpath, "__init__pyfile"))
+
+    return flag_root_dir_has_init_pyfile
 
 
 def search_tocompile_files(search_dir: str, 
                            exclude_files=None, exclude_dirs=None) -> list:
+    orig_cwd = os.getcwd()
+    os.chdir(search_dir)
+
     exclude_file_list = exclude_files or []
     exclude_dir_list = exclude_dirs or []
 
-    search_ext_list = [
-        "py", "pY", "Py", "PY", 
-        "pyx", "pyX", "pYx", "pYX", "Pyx", "PyX", "PYx", "PYX",
-    ]
 
     tocompile_file_list = []
-    for search_ext in search_ext_list:
-        search_glob_pattern = os.path.join(search_dir, f"**/*.{search_ext}")
+    for search_ext in _tocompile_ext_list:
+        search_glob_pattern = os.path.join(f"**{os.sep}*.{search_ext}")
         for filepath in glob.iglob(search_glob_pattern, recursive=True):
+            
             filename = os.path.basename(filepath)
             lowcase_filename = filename.lower()
             if (lowcase_filename == "setup.py") \
@@ -77,6 +110,7 @@ def search_tocompile_files(search_dir: str,
                 continue
 
             tocompile_file_list.append(filepath)
-
+    
+    os.chdir(orig_cwd)
     return tocompile_file_list
 
